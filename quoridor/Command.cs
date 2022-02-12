8c25b83da89edde53512f9bc0688411ab0ed1091
artur <vypovskyy.pr@gmail.com>
2022-02-12 19:15:22 +0200

    new files added

---
+++ quoridor/Command.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace quoridor
+{
+    public class Command
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int ToCol { get; set; } = -1;
+
+        public int ToRow { get; set; } = -1;
+
+        public char? Orientation { get; set; } = null;
+
+
+        public Command(string name = "", int toRow = -1, int toCol = -1, char? orientation = null)
+        {
+            Name = name;
+            ToCol = toCol;
+            ToRow = toRow;
+            Orientation = orientation;
+        }
+
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+
+        public void SetCoordinates(int toRow, int toCol)
+        {
+            ToCol = toCol;
+            ToRow = toRow;
+        }
+
+
+        public void SetOrientation(char orientation)
+        {
+            Orientation = orientation;
+        }
+    }
+}
+
